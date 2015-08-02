00da1b80f9b0a56d284b44e98adbc45b73c8958a
Davin <davinmerry@hotmail.com>
2015-08-02 15:46:18 -0500

    Condensed init.lua / Added config
    Removed some debug code, modified code, and added a config (currently
    has 1 setting).

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
+--[[
+Condensed Blocks
+
+Created by mzx987
+]]
+
+
+
 --List of blocks used from "default" mod.
 local blockList = {
 "brick", "bronzeblock", "clay",
@@ -27,127 +35,74 @@ local imageList = {
 "stone_brick", "wood"
 }
 
+--Load config
+local path = minetest.get_modpath("condensedblocks")
+dofile(path.."/config.txt")
+
 -----------
 --Main Loop
 -----------
 
-local testNode = minetest.registered_nodes["default:"..blockList[13]]
-for k, v in pairs(testNode) do print(k, v) end
-print(testNode["drawtype"])
-
 for i = 1, #blockList do
+
   local node = minetest.registered_nodes["default:"..blockList[i]]
-  print("[Condensed Blocks] Creating "..node["description"].."... ")
+  print("[Condensed Blocks] Creating "..node["description"].."... ") --Used for debugging purposes.
   
   --Checks to see if the node is glass (used to prevent X-Ray blocks)
   local glass = {}
   if node["drawtype"] == "glasslike" or node["drawtype"] == "glasslike_framed_optional" then
     glass = {"glasslike", "light", true} else glass = {"normal", "none", false}
   end
-
-  minetest.register_node("condensedblocks:"..blockList[i].."_x1", {
-    tiles = {"default_"..imageList[i]..".png^condensedblocks_x1.png"},
-    description = node["description"].." x1 (9)",
-    drawtype = glass[1],
-    paramtype = glass[2],
-    sunlight_propagates = glass[3],
-    groups = {cracky=2, stone=1, oddly_breakable_by_hand=2},
-    sounds = default.node_sound_stone_defaults(),
-  })
-
-  minetest.register_node("condensedblocks:"..blockList[i].."_x2", {
-    tiles = {"default_"..imageList[i]..".png^condensedblocks_x2.png"},
-    description = node["description"].." x2 (81)",
-    drawtype = glass[1],
-    paramtype = glass[2],
-    sunlight_propagates = glass[3],
-    groups = {cracky=1, stone=1, oddly_breakable_by_hand=1},
-    sounds = default.node_sound_stone_defaults(),
-  })
-
-  minetest.register_node("condensedblocks:"..blockList[i].."_x3", {
-    tiles = {"default_"..imageList[i]..".png^condensedblocks_x3.png"},
-    description = node["description"].." x3 (729)",
-    drawtype = glass[1],
-    paramtype = glass[2],
-    sunlight_propagates = glass[3],
-    groups = {cracky=1, stone=1, oddly_breakable_by_hand=1},
-    sounds = default.node_sound_stone_defaults(),
-  })
-
-  minetest.register_node("condensedblocks:"..blockList[i].."_x4", {
-    tiles = {"default_"..imageList[i]..".png^condensedblocks_x4.png"},
-    description = node["description"].." x4 (6561)",
-    drawtype = glass[1],
-    paramtype = glass[2],
-    sunlight_propagates = glass[3],
-    groups = {cracky=1, stone=1, oddly_breakable_by_hand=1},
-    sounds = default.node_sound_stone_defaults(),
-  })
   
+  for conVal = 1, condensedblocks_multiplier do
+
+    minetest.register_node("condensedblocks:"..blockList[i].."_x"..conVal, {
+      tiles = {"default_"..imageList[i]..".png^condensedblocks_x"..conVal..".png"},
+      description = node["description"].." x"..conVal.." ("..(9 ^ conVal)..")",
+      drawtype = glass[1],
+      paramtype = glass[2],
+      sunlight_propagates = glass[3],
+      groups = {cracky=1, stone=1, oddly_breakable_by_hand=1},
+      sounds = default.node_sound_stone_defaults(),
+    })
+
 ---------
 --Recipes
 ---------
-  minetest.register_craft({
-    output = "condensedblocks:"..blockList[i].."_x1 1",
-    recipe = {
-      {"default:"..blockList[i], "default:"..blockList[i], "default:"..blockList[i]},
-      {"default:"..blockList[i], "default:"..blockList[i], "default:"..blockList[i]},
-      {"default:"..blockList[i], "default:"..blockList[i], "default:"..blockList[i]},
-    }
-  })
 
-  minetest.register_craft({
-    output = "condensedblocks:"..blockList[i].."_x2 1",
-    recipe = {
-      {"condensedblocks:"..blockList[i].."_x1", "condensedblocks:"..blockList[i].."_x1", "condensedblocks:"..blockList[i].."_x1"},
-      {"condensedblocks:"..blockList[i].."_x1", "condensedblocks:"..blockList[i].."_x1", "condensedblocks:"..blockList[i].."_x1"},
-      {"condensedblocks:"..blockList[i].."_x1", "condensedblocks:"..blockList[i].."_x1", "condensedblocks:"..blockList[i].."_x1"},
-    }
-  })
+    if conVal == 1 then
+      minetest.register_craft({
+        output = "condensedblocks:"..blockList[i].."_x1 1",
+        recipe = {
+          {"default:"..blockList[i], "default:"..blockList[i], "default:"..blockList[i]},
+          {"default:"..blockList[i], "default:"..blockList[i], "default:"..blockList[i]},
+          {"default:"..blockList[i], "default:"..blockList[i], "default:"..blockList[i]},
+        }
+      })
+      
+      minetest.register_craft({
+        type = "shapeless",
+        output = "default:"..blockList[i].." 9",
+        recipe = {"condensedblocks:"..blockList[i].."_x1"},
+      })
+    
+    else
+      minetest.register_craft({
+        output = "condensedblocks:"..blockList[i].."_x"..conVal.." 1",
+        recipe = {
+          {"condensedblocks:"..blockList[i].."_x"..(conVal - 1), "condensedblocks:"..blockList[i].."_x"..(conVal - 1), "condensedblocks:"..blockList[i].."_x"..(conVal - 1)},
+          {"condensedblocks:"..blockList[i].."_x"..(conVal - 1), "condensedblocks:"..blockList[i].."_x"..(conVal - 1), "condensedblocks:"..blockList[i].."_x"..(conVal - 1)},
+          {"condensedblocks:"..blockList[i].."_x"..(conVal - 1), "condensedblocks:"..blockList[i].."_x"..(conVal - 1), "condensedblocks:"..blockList[i].."_x"..(conVal - 1)},
+        }
+      })
 
-  minetest.register_craft({
-    output = "condensedblocks:"..blockList[i].."_x3 1",
-    recipe = {
-      {"condensedblocks:"..blockList[i].."_x2", "condensedblocks:"..blockList[i].."_x2", "condensedblocks:"..blockList[i].."_x2"},
-      {"condensedblocks:"..blockList[i].."_x2", "condensedblocks:"..blockList[i].."_x2", "condensedblocks:"..blockList[i].."_x2"},
-      {"condensedblocks:"..blockList[i].."_x2", "condensedblocks:"..blockList[i].."_x2", "condensedblocks:"..blockList[i].."_x2"},
-    }
-  })
-
-  minetest.register_craft({
-    output = "condensedblocks:"..blockList[i].."_x4 1",
-    recipe = {
-      {"condensedblocks:"..blockList[i].."_x3", "condensedblocks:"..blockList[i].."_x3", "condensedblocks:"..blockList[i].."_x3"},
-      {"condensedblocks:"..blockList[i].."_x3", "condensedblocks:"..blockList[i].."_x3", "condensedblocks:"..blockList[i].."_x3"},
-      {"condensedblocks:"..blockList[i].."_x3", "condensedblocks:"..blockList[i].."_x3", "condensedblocks:"..blockList[i].."_x3"},
-    }
-  })
-
-  minetest.register_craft({
-    type = "shapeless",
-    output = "default:"..blockList[i].." 9",
-    recipe = {"condensedblocks:"..blockList[i].."_x1"},
-  })
-
-  minetest.register_craft({
-    type = "shapeless",
-    output = "condensedblocks:"..blockList[i].."_x1 9",
-    recipe = {"condensedblocks:"..blockList[i].."_x2"},
-  })
-
-  minetest.register_craft({
-    type = "shapeless",
-    output = "condensedblocks:"..blockList[i].."_x2 9",
-    recipe = {"condensedblocks:"..blockList[i].."_x3"},
-  })
-
-  minetest.register_craft({
-    type = "shapeless",
-    output = "condensedblocks:"..blockList[i].."_x3 9",
-    recipe = {"condensedblocks:"..blockList[i].."_x4"},
-  })
-
+      minetest.register_craft({
+        type = "shapeless",
+        output = "condensedblocks:"..blockList[i].."_x"..(conVal - 1).." 9",
+        recipe = {"condensedblocks:"..blockList[i].."_x"..conVal},
+      })
+    end
+  end
 end
 
 print("[Condensed Blocks] Blocks loaded sucessfully!")
